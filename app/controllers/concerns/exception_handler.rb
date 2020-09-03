02c3b180f4d080bb4c6b363ddb647bfe1f66f13d
fedgut <fedgut@gmail.com>
2020-09-03 15:28:56 -0500

    add jwt config and expection handling

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,7 +1,16 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
@@ -9,5 +18,15 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
+  end
+
+  private
+
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  def unauthorized_request(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
