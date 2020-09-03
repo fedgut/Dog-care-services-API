104173574997367f3890e7dbc840c100abae6809
fedgut <fedgut@gmail.com>
2020-09-03 17:19:20 -0500

    Create files to auth user via email and pass

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :favourites, only: [:create, :destroy, :index] 
   end
   resources :services
+  post 'auth/login', to: 'authentication#authenticate'
 end
